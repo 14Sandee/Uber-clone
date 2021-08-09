4dbee0227609d7232d7a6089ce24ab3725611bcb
14Sandee <55476652+14Sandee@users.noreply.github.com>
2021-08-09 12:36:31 +0530

    Uber first commit

---
+++ screens/MapScreen.js
@@ -0,0 +1,53 @@
+import React from 'react';
+import { StyleSheet, Text, View, SafeAreaView, TouchableOpacity } from 'react-native';
+import tw from "tailwind-react-native-classnames";
+import Map from '../components/Map';
+import { createStackNavigator } from '@react-navigation/stack';
+import NavigateCard from '../components/NavigateCard';
+import RideOptionCard from '../components/RideOptionCard';
+import { Icon } from "react-native-elements/dist/icons/Icon"
+import { useNavigation } from '@react-navigation/core';
+
+const MapScreen = () => {
+
+    const Stack = createStackNavigator();
+    const navigation = useNavigation();
+
+    return (
+        <View>
+
+            <TouchableOpacity 
+            onPress={() => navigation.navigate("HomeScreen")}
+            style={tw`absolute top-10 left-6 bg-gray-100 z-50 p-3 rounded-full shadow-lg`}>
+                <Icon name="menu" />
+            </TouchableOpacity>
+            <View style={tw`h-1/2`}>
+                <Map />
+            </View>
+
+            <View style={tw`h-1/2`}>
+                <Stack.Navigator>
+                    <Stack.Screen 
+                        name="NavigateCard"
+                        component={NavigateCard}
+                        options={{
+                            headerShown: false,
+                        }}
+                    />
+                    <Stack.Screen 
+                        name="RideOptionCard"
+                        component={RideOptionCard}
+                        options={{
+                            headerShown: false,
+                        }}
+                    />
+                </Stack.Navigator>
+            </View>
+        </View>
+        
+    )
+}
+
+export default MapScreen
+
+const styles = StyleSheet.create({})
